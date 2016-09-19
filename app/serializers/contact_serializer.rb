ee73fb2767e878ffafdd13e4c4226ffeefd1b129
Zlatko Alomerovic <zlatko@miavision.net>
2016-09-19 14:28:10 +0200

    Fix serializer

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,6 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :family_name, :given_name, :title, :phone, :email, :website, :address, :customer_id, :additional_info
-  has_one :company
+  belongs_to :company
   has_many :notes
   has_many :projects
 end
